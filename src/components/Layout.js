0d5367f360672922cb92858d5c677e197adbb09d
Michaela Greiler-Göritzer <michaela.greiler@gmail.com>
2021-10-06 17:13:26 +0200

    Seperate header and postlist using css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,17 +6,42 @@ import * as styles from "../styles/home.module.css";
 
 export default function Layout({ children }) {
   return (
-    <section className={styles.header}>
-      <main>
-        <title>Home Page</title>
-        <div className="layout">
-          <Navbar />
-          <div className="content">{children}</div>
-          <footer>
-            <p>Copyright 2021 Michaela Greiler</p>
-          </footer>
-        </div>
-      </main>
-    </section>
+    <main>
+      <section className={styles.header}>
+          <title>SE Unlocked</title>
+          <div className="layout">
+            <Navbar />
+
+          </div>
+      </section>
+      <section className={styles.body}>
+                  <div className="content">{children}</div>
+      </section>
+      <section>
+            <footer>
+              <p>Copyright 2021 Michaela Greiler</p>
+            </footer>
+      </section>
+    </main>
   );
 }
+
+
+
+
+// export default function Layout({ children }) {
+//   return (
+//     <section className={styles.header}>
+//       <main>
+//         <title>SE Unlocked</title>
+//         <div className="layout">
+//           <Navbar />
+//           <div className="content">{children}</div>
+//           <footer>
+//             <p>Copyright 2021 Michaela Greiler</p>
+//           </footer>
+//         </div>
+//       </main>
+//     </section>
+//   );
+// }
