d68ef87a7c60f1b18b5b19def193593017fc14fb
MussadiqKhan <34172073+MussadiqKhan@users.noreply.github.com>
2021-12-12 15:46:26 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,13 @@ module.exports = {
     //     },
     //   },
     // },
+    {
+      resolve: "gatsby-plugin-mailchimp",
+      options: {
+        endpoint: "", // string; add your MC list endpoint here; see instructions below
+        timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
+      },
+    },
     "gatsby-plugin-image",
     {
       resolve: "gatsby-plugin-google-analytics",
