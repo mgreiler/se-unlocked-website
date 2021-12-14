b3f37e8b4b91854cf28369de8869558d897a2675
MussadiqKhan <34172073+MussadiqKhan@users.noreply.github.com>
2021-12-15 01:58:21 +0500

    Update Newsletter.js

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import addToMailchimp from "gatsby-plugin-mailchimp";
 
 export default function Newsletter() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await addToMailchimp(email);
+    console.log(result);
+    setMessage(result.msg);
+    // I recommend setting `result` to React state
+    // but you can do whatever you want
+  };
+
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   return (
     <>
       <div className="newsletter flex flex-column align-items-center justify-content-center">
@@ -10,9 +22,24 @@ export default function Newsletter() {
           episode. In addition, she shares behind-the-scenes information about
           the show, the guests, and her journey as a tech entrepreneur.
         </p>
-        <form className="flex flex-column align-items-center">
-          <input type="email" placeholder="Email" required />{" "}
+        <form
+          onSubmit={(e) => handleSubmit(e)}
+          className="flex flex-column align-items-center"
+        >
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            onChange={(e) => {
+              setEmail(e.currentTarget.value);
+            }}
+          />{" "}
           <input type="text" placeholder="First Name" required />
+          {message ? (
+            <p style={{ color: "white", fontSize: "16px" }}>{message}</p>
+          ) : (
+            ""
+          )}
           <button>Subscribe</button>
         </form>
       </div>
